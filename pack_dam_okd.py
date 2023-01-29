cebcb7d4e437f1aa87b387ebcf59d6cab8f15eec
soltia48 <soltia48@gmail.com>
2023-01-29 22:12:54 +0900

    Update pack_dam_okd.py

--- pack_dam_okd.py
+++ pack_dam_okd.py
@@ -19,54 +19,56 @@ from dam_okd_utility.okd_p3_track_info_chunk import (
 from dam_okd_utility.okd_p_track_chunk import OkdPTrackChunk
 
 
-class DamOkdCreator:
-    __logger = getLogger("DamOkdCreator")
+class DamOkdPacker:
+    __logger = getLogger("DamOkdPacker")
 
     @staticmethod
     def load_json(stream: io.BufferedReader):
-        try:
-            p3_track_info_chunk: OkdP3TrackInfoChunk = simplejson.load(
-                stream, object_hook=OkdP3TrackInfoChunk.from_json_object
-            )
-            DamOkdCreator.__logger.info("P3-Track Information loaded.")
-            return p3_track_info_chunk
-        except simplejson.JSONDecodeError:
-            pass
+        raw_json_object = simplejson.load(stream)
 
-        try:
+        stream.seek(0)
+        if "data" in raw_json_object:
             p_track_info_chunk: OkdPTrackInfoChunk = simplejson.load(
                 stream, object_hook=OkdPTrackInfoChunk.from_json_object
             )
-            DamOkdCreator.__logger.info("P-Track Information loaded.")
+            DamOkdPacker.__logger.info("P-Track Information loaded.")
             return p_track_info_chunk
-        except simplejson.JSONDecodeError:
-            pass
+        elif "track_number" in raw_json_object:
+            p3_track_info_chunk: OkdP3TrackInfoChunk = simplejson.load(
+                stream, object_hook=OkdP3TrackInfoChunk.from_json_object
+            )
+            DamOkdPacker.__logger.info("P3-Track Information loaded.")
+            return p3_track_info_chunk
 
     @staticmethod
     def load_file(path: str):
         mime_type: tuple[(str | None), (str | None)] = mimetypes.guess_type(path)
         with open(path, "rb") as input_file:
             if mime_type[0] == "application/json":
-                return DamOkdCreator.load_json(input_file)
+                return DamOkdPacker.load_json(input_file)
             elif mime_type[0] == "audio/sp-midi":
                 midi = mido.MidiFile(file=input_file)
-                DamOkdCreator.__logger.info("P-Track loaded.")
-                return OkdPTrackChunk.from_midi(midi)
+                p_track_chunk = OkdPTrackChunk.from_midi(midi)
+                DamOkdPacker.__logger.info("P-Track loaded.")
+                return p_track_chunk
 
         raise ValueError(f"Unknown file type detected. mime_type={mime_type[0]}")
 
     @staticmethod
-    def create(
+    def pack(
         output_stream: io.BufferedWriter,
         *input_file_paths: str,
     ):
         chunks: list[OkdChunk] = []
         for input_file_path in input_file_paths:
-            chunk = DamOkdCreator.load_file(input_file_path)
+            chunk = DamOkdPacker.load_file(input_file_path)
             if chunk is None:
                 continue
 
-            chunks.extend(chunk)
+            if isinstance(chunk, list):
+                chunks.extend(chunk)
+            else:
+                chunks.append(chunk)
 
         OkdFile.encrypt(output_stream, chunks)
 
@@ -78,7 +80,7 @@ def main(argv=None):
     args = parser.parse_args()
 
     with open(args.output_path, "wb") as output_file:
-        DamOkdCreator.create(output_file, *args.input_path)
+        DamOkdPacker.pack(output_file, *args.input_path)
 
 
 if __name__ == "__main__":
