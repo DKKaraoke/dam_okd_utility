012c5c6b46e683da123b0b4561c9b632e96af582
soltia48 <soltia48@gmail.com>
2023-02-02 05:08:27 +0900

    Refactor test

--- test/test_okd_midi.py
+++ test/test_okd_midi.py
@@ -17,13 +17,13 @@ class TestOkdMidi(unittest.TestCase):
         (0x04103F, b"\x7f\x7f\x3f"),
     ]
     EXTENDED_VALUES: list[tuple[int, bytes]] = [
-        (0x000000, b"\x00\x80"),
-        (0x00003F, b"\x3f\x80"),
-        (0x00103F, b"\x7f\x3f\x80"),
-        (0x04103F, b"\x7f\x7f\x3f\x80"),
-        (0x04107E, b"\x7f\x7f\x3f\x3f\x80"),
-        (0x04207E, b"\x7f\x7f\x3f\x7f\x3f\x80"),
-        (0x08207E, b"\x7f\x7f\x3f\x7f\x7f\x3f\x80"),
+        (0x000000, b"\x00"),
+        (0x00003F, b"\x3f"),
+        (0x00103F, b"\x7f\x3f"),
+        (0x04103F, b"\x7f\x7f\x3f"),
+        (0x04107E, b"\x7f\x7f\x3f\x3f"),
+        (0x04207E, b"\x7f\x7f\x3f\x7f\x3f"),
+        (0x08207E, b"\x7f\x7f\x3f\x7f\x7f\x3f"),
     ]
 
     def test_read_varibale_int(self):
@@ -54,7 +54,7 @@ class TestOkdMidi(unittest.TestCase):
     def test_read_extended_variable_int(self):
         for value, buffer in TestOkdMidi.EXTENDED_VALUES:
             with self.subTest(value=value, buffer=buffer):
-                stream = bitstring.BitStream(buffer)
+                stream = bitstring.BitStream(buffer + b"\x80")
                 read_value = read_extended_variable_int(stream)
                 self.assertEqual(value, read_value)
 
@@ -64,8 +64,6 @@ class TestOkdMidi(unittest.TestCase):
                 stream = bitstring.BitStream()
                 write_extended_variable_int(stream, value)
                 written_buffer: bytes = stream.read("bytes")
-                # Add a status byte
-                written_buffer += b"\x80"
                 self.assertEqual(buffer, written_buffer)
 
 
